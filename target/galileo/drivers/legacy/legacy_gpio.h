2ddd45c2de686deb573e5d551c15c7abe8cffe96
Ran Bao <ran.bao@wirelessguard.co.nz>
2017-05-10 21:47:29 +1200

    Add comment for legacy GPIO driver

--- target/galileo/drivers/legacy/legacy_gpio.h
+++ target/galileo/drivers/legacy/legacy_gpio.h
@@ -22,12 +22,41 @@ typedef struct
 extern "C" {
 #endif
 
+/**
+ * Initialize a leacy GPIO port
+ * @param obj The GPIO object
+ * @param pin Pin number [0 ... 5]
+ * @param direction Input or output
+ * @param value Output high or low, represented by 1 and 0
+ */
 void legacy_gpio_init(legacy_gpio_t *obj, uint32_t pin, uint32_t direction, uint32_t value);
 
+/**
+ * Read from a legacy GPIO port
+ * @param obj The GPIO object
+ * @return 0 for input low, non-zero for input high
+ */
 uint32_t legacy_gpio_read(legacy_gpio_t *obj);
+
+/**
+ * Write to a legacy GPIO port
+ * @param obj The GPIO object
+ * @param value 0 for output low, 1 for ouput high
+ */
 void legacy_gpio_write(legacy_gpio_t *obj, uint32_t value);
 
+/**
+ * Read data direction for a legacy GPIO port
+ * @param obj The GPIO object
+ * @return 0 for output, non-zero for input
+ */
 uint32_t legacy_gpio_get_direction(legacy_gpio_t *obj);
+
+/**
+ * Set data direction for a legacy GPIO port
+ * @param obj The GPIO object
+ * @param direction 0 for output, 1 for input
+ */
 void legacy_gpio_set_direction(legacy_gpio_t *obj, uint32_t direction);
 
 #ifdef __cplusplus

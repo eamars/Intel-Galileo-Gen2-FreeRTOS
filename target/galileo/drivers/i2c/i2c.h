33bd436fb01288a5120ea8ec12e3fd5718f8a635
Ran Bao <ran.bao@wirelessguard.co.nz>
2017-05-11 23:30:34 +1200

    Add prototype for I2C

---
+++ target/galileo/drivers/i2c/i2c.h
@@ -0,0 +1,25 @@
+/**
+ * @file i2c.h
+ * @brief I2C controller driver
+ * @author Ran Bao
+ * @date May, 2017
+ */
+
+#ifndef I2C_H_
+#define I2C_H_
+
+typedef struct
+{
+
+} i2c_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* I2C_H_ */
